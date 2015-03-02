25246b8057c9dcdef91d88d36062690929a2aed9
Nathan Eidelson <nathaneidelson@DN0a232093.SUNet>
2015-03-02 13:51:28 -0800

    added processing file

--- eggio.ino
+++ eggio.ino
@@ -1,9 +1,20 @@
-void setup() {
-  // put your setup code here, to run once:
-
+void setup() {  
+  // Set up input
+  // Set up output pin (vibration motor)
+  
+  // initialize the serial communication:
+  Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+
+void loop() {
+
+  // Read values of sensors
+  // Record values
+  // Send to processing
+  
+  Serial.println("3");
+  delay(10);
+  
 }
